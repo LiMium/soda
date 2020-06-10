81aacb429d8a1905bac616fd9d24db0e7bda155e
hrj <hrj@lavadip.com>
2020-06-10 21:28:15 +0530

    minor

--- src/main/scala/soda/Main.scala
+++ src/main/scala/soda/Main.scala
@@ -10,9 +10,10 @@ object Main {
     // val url = new URL("http://localhost:9000/grinder/nightly-unstable/xhtml1/reference/absolute-non-replaced-width-003-ref.xht")
     val url = new URL("http://localhost:8000//nightly-unstable/xhtml1/abspos-containing-block-005.xht")
 
-    // println(dom.walkAllElements().mkString(","))
     val outImg = Renderer.render(url, "")
 
     ImageIO.write(outImg, "PNG", new File("urlOut.png"))
+
+    println(url)
   }
 }
